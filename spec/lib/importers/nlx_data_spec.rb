9020c6dffce66c3664fabae1817e6dd3c82d416b
Greg Gershman <greg@shelrick.com>
2014-07-11 13:25:03 -0400

    Fix failing spec.

--- spec/lib/importers/nlx_data_spec.rb
+++ spec/lib/importers/nlx_data_spec.rb
@@ -11,18 +11,21 @@ describe NlxData do
          position_title: 'Manufacturing Engineer', tags: %w(federal),
          organization_id: 'AB', organization_name: 'Allied-Barton',
          locations: [{city: 'Tijuana', state: 'MD'}],
-         start_date: Date.parse('2014-07-09'), end_date: Date.parse('2014-07-09') + 30.days },
+         start_date: Date.parse('2014-07-09'), end_date: Date.parse('2014-07-09') + 30.days,
+         url: "http://eaton-veterans.jobs/59D6C80902EC4FC88EF01BADBB990FBB24" },
         {type: 'position_opening', source: 'alliedbarton', external_id: "E48345D53CF44992AFF406CFFFF2BB42", _ttl: ttl,
          position_title: 'Service Sales Engineer', tags: %w(federal),
          organization_id: 'AB', organization_name: 'Allied-Barton',
          locations: [{city: 'Nanterre', state: 'TX'}],
-         start_date: Date.parse('2014-07-09'), end_date: Date.parse('2014-07-09') + 30.days 
+         start_date: Date.parse('2014-07-09'), end_date: Date.parse('2014-07-09') + 30.days,
+         url: "http://eaton-veterans.jobs/E48345D53CF44992AFF406CFFFF2BB4224" 
         },
         {type: 'position_opening', source: 'alliedbarton', external_id: "C4677B65BBEB47B7AF7923DBCDC81B38", _ttl: ttl,
          position_title: 'Production Technician - Glendale Heights, IL', tags: %w(federal),
          organization_id: 'AB', organization_name: 'Allied-Barton',
          locations: [{city: 'Glendale Heights', state: 'IL'}],
-         start_date: Date.parse('2014-07-09'), end_date: Date.parse('2014-07-09') + 30.days 
+         start_date: Date.parse('2014-07-09'), end_date: Date.parse('2014-07-09') + 30.days,
+         url: "http://eaton-veterans.jobs/C4677B65BBEB47B7AF7923DBCDC81B3824" 
         }         
       ]
     end
