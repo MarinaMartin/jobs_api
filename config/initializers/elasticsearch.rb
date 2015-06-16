79791e7bca4693e0946cfe5020bf562ed3d0fa64
Mike Auclair <mike@mikeauclair.com>
2015-06-16 09:52:47 -0400

    don't use blank

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,5 +1,5 @@
 module Elasticsearch
   es_config = (YAML.load_file("#{Rails.root}/config/elasticsearch.yml") || {})[Rails.env]
   INDEX_NAME = es_config && es_config['index_name'].present? ? es_config['index_name'].freeze : "#{Rails.env}:jobs:head".freeze
-  WRITE_INDEX_NAME = ENV['BONSAI_WRITE'] || INDEX_NAME
+  WRITE_INDEX_NAME = ENV['BONSAI_WRITE'].presence || INDEX_NAME
 end
